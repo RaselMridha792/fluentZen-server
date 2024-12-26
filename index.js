8ed9b263e7d81471bc0dd1e29be2c58f549d7480
rasel mridha <raselmridha792@gmail.com>
2024-12-26 17:51:57 +0600

    create jwt token and verifyToken for secure api and get secret key from client site and complete the deployment process

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-require('dotenv').config();
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://assignment-11-2478a.web.app",
+      "https://assignment-11-2478a.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
-const uri =`mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.epzmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.epzmh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,80 +34,135 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
+    // create db collection
+    const tutorialsCollections = client
+      .db("FluentZen_admin")
+      .collection("FluentZen_Tutorials");
+    const bookedTutorCollections = client
+      .db("FluentZen_admin")
+      .collection("my_booked_tutor");
 
+    // jwt related api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV=== 'production',
+          sameSite: process.env.NODE_ENV=== 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true });
+    });
 
-    // create db collection 
-    const tutorialsCollections = client.db("FluentZen_admin").collection("FluentZen_Tutorials");
-    const bookedTutorCollections = client.db("FluentZen_admin").collection("my_booked_tutor");
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure:  process.env.NODE_ENV=== 'production',
+          sameSite: process.env.NODE_ENV=== 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true });
+    });
 
+    // verify token middleware function
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies.token;
 
-    
-    // post tutorials data on db 
-    app.post('/add-tutorials', async(req, res)=>{
-        const tutorial = req.body;
-        const result = await tutorialsCollections.insertOne(tutorial);
-        res.send(result);
-    })
+      if (!token) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
 
-    // get all tutors 
-    app.get('/find-tutors', async(req, res)=>{
-      const tutors = tutorialsCollections.find()
-      const result = await tutors.toArray()
-      res.send(result)
-    })
+      // verify token
+      if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: "unauthorized token" });
+          }
+          req.user = decoded;
+          next();
+        });
+      }
+    };
+
+    // post tutorials data on db
+    app.post("/add-tutorials", verifyToken, async (req, res) => {
+      const tutorial = req.body;
+      const result = await tutorialsCollections.insertOne(tutorial);
+      res.send(result);
+    });
+
+    // get all tutors
+    app.get("/find-tutors", async (req, res) => {
+      const tutors = tutorialsCollections.find();
+      const result = await tutors.toArray();
+      res.send(result);
+    });
 
     // find tutorial by id
-    app.get("/tutor/details/:id", async(req, res)=>{
+    app.get("/tutor/details/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await tutorialsCollections.findOne(query);
       res.send(result);
-    })
-
+    });
 
     // add booked tutor in a new collection
-    app.post("/booked-tutor", async(req, res)=>{
+    app.post("/booked-tutor", verifyToken, async (req, res) => {
       const tutors = req.body;
       const result = await bookedTutorCollections.insertOne(tutors);
       res.send(result);
-    })
+    });
 
-    app.get('/my-booked-tutor', async(req, res)=>{
+    app.get("/my-booked-tutor", verifyToken, async (req, res) => {
       const user = req.query.email;
-      const query = {email: user}
-      const tutors = bookedTutorCollections.find(query)
-      const result = await tutors.toArray()
-      res.send(result)
-    })
+      const query = { email: user };
+
+      if (req.user.email !== user) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const tutors = bookedTutorCollections.find(query);
+      const result = await tutors.toArray();
+      res.send(result);
+    });
 
     // find my add tutorials by using email
-    app.get("/my-tutorials", async(req, res)=>{
+    app.get("/my-tutorials", verifyToken, async (req, res) => {
       const user = req.query.email;
-      const query = {email: user}
-      const result = await tutorialsCollections.find(query).toArray()
-      res.send(result)
-    })
+      const query = { email: user };
 
+      if (req.user.email !== user) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await tutorialsCollections.find(query).toArray();
+      res.send(result);
+    });
 
     // delete tutorials from collection by searching id
-    app.delete("/delete-tutorial/:id", async(req, res)=>{
+    app.delete("/delete-tutorial/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const result = await tutorialsCollections.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await tutorialsCollections.deleteOne(query);
+      res.send(result);
+    });
 
-    // update a tutorial user added from the add tutorial page 
-    app.patch('/update-tutorial/:id', async(req, res)=>{
+    // update a tutorial user added from the add tutorial page
+    app.patch("/update-tutorial/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const tutorial = req.body;
-      const filter = {_id : new ObjectId(id)}
-      const options = {upsert: false}
+      const filter = { _id: new ObjectId(id) };
+
+      if (req.user.email !== tutorial.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const options = { upsert: false };
       const updatedTutorial = {
-        $set:{
+        $set: {
           name: tutorial.name,
           email: tutorial.email,
           image: tutorial.image,
@@ -103,39 +170,65 @@ async function run() {
           price: tutorial.price,
           description: tutorial.description,
           review: tutorial.review,
-        }
-      }
-      const result = await tutorialsCollections.updateOne(filter, updatedTutorial, options);
-      res.send(result)
+        },
+      };
+      const result = await tutorialsCollections.updateOne(
+        filter,
+        updatedTutorial,
+        options
+      );
+      res.send(result);
     });
 
     // update the review bassed on user Given Rivew
-    app.patch("/tutor-review/:id", async(req, res)=>{
+    app.patch("/tutor-review/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const tutorial = await tutorialsCollections.findOne(filter)
-      const reviewValue = tutorial.review+1
-      const options = {upsert: false}
+      const filter = { _id: new ObjectId(id) };
+      const tutorial = await tutorialsCollections.findOne(filter);
+      const reviewValue = tutorial.review + 1;
+      const options = { upsert: false };
       const updatedReview = {
-        $set:{
+        $set: {
           review: reviewValue,
-        }
-      }
-      const result = await tutorialsCollections.updateOne(filter, updatedReview, options);
-      res.send(result)
-    })
+        },
+      };
+      const result = await tutorialsCollections.updateOne(
+        filter,
+        updatedReview,
+        options
+      );
+      res.send(result);
+    });
 
-    // get data using categories 
-    app.get("/find-tutors/:category", async(req, res)=>{
+    // for booked database update
+    app.patch("/booked-tutor-review/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { tutor_id: id };
+      const tutorial = await bookedTutorCollections.findOne(filter);
+      const reviewValue = tutorial.review + 1;
+      const options = { upsert: false };
+      const updatedReview = {
+        $set: {
+          review: reviewValue,
+        },
+      };
+      const result = await bookedTutorCollections.updateOne(
+        filter,
+        updatedReview,
+        options
+      );
+      res.send(result);
+    });
+
+    // get data using categories
+    app.get("/find-tutors/:category", async (req, res) => {
       const category = req.params.category;
-      const query = {language: category}
-      const result = await tutorialsCollections.find(query).toArray()
-      res.send(result)
-    })
+      const query = { language: category };
+      const result = await tutorialsCollections.find(query).toArray();
+      res.send(result);
+    });
 
-
-
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
